ab5d73d083b87f0f1429ee98fcf46ea5d4bd3816
sajad <sajadsajad.sch@gmail.com>
2021-07-07 04:41:07 -0400

    hiden password input

--- app/PASSWDB.py
+++ app/PASSWDB.py
@@ -146,111 +146,114 @@ def hash(sname,name,passwd):
 
 # account creator 
 def account():
-	os.system("clear") if os.name == "posix" else os.system("cls")
-	logo()
-	pwd=os.getcwd()
-	name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX+"")
-	passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-	print(Fore.LIGHTBLUE_EX+" ok !, please wite ...")
-	import hashlib
-	hsh=hashlib.md5()
-	hsh.update(passwd.encode("utf-8"))
-	hash=hsh.hexdigest()
-	import sqlite3
-	db=sqlite3.connect(".li.db")
-	
-	try:
-		db.execute("CREATE TABLE li (name varchar(20),password INT,hashs varchar(256))")
-		db.execute("INSERT INTO li VALUES('%s','%s','%s')"%(name,passwd,hash))
-		db.commit()
-		print(Fore.LIGHTBLUE_EX+" Query 1,OK")
-		db.close()
-	except Exception:
-		db.execute("INSERT INTO li VALUES('%s','%s','%s')"%(name,passwd,hash))
-		db.commit()
-	except:
-		print(Fore.LIGHTRED_EX+"➜"+Fore.LIGHTBLUE_EX+" this account is already exists !")
-	finally:
-		login()
+    os.system("clear") if os.name == "posix" else os.system("cls")
+    logo()
+    pwd=os.getcwd()
+    name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX+"")
+	#passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+    passwd = getpass(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX+"")
+    print(Fore.LIGHTBLUE_EX+" ok !, please wite ...")
+    import hashlib
+    hsh=hashlib.md5()
+    hsh.update(passwd.encode("utf-8"))
+    hash=hsh.hexdigest()
+    import sqlite3
+    db=sqlite3.connect(".li.db")
+    
+    try:
+	    db.execute("CREATE TABLE li (name varchar(20),password INT,hashs varchar(256))")
+	    db.execute("INSERT INTO li VALUES('%s','%s','%s')"%(name,passwd,hash))
+	    db.commit()
+	    print(Fore.LIGHTBLUE_EX+" Query 1,OK")
+	    db.close()
+    except Exception:
+	    db.execute("INSERT INTO li VALUES('%s','%s','%s')"%(name,passwd,hash))
+	    db.commit()
+    except:
+        print(Fore.LIGHTRED_EX+"➜"+Fore.LIGHTBLUE_EX+" this account is already exists !")
+    finally:
+        login()
 		
 def login():
-	from time import sleep 
-	os.system("clear") if os.name == "posix" else os.system("cls")
-	pwd=os.getcwd()
-	logo()
-	print(Fore.LIGHTBLUE_EX+" WELCOME TO PASSWDB ! :)\n")
-	incm=["[0]- login ","[1]- account"]
-	for e in incm:
-		print(Fore.YELLOW+" ➜"+Fore.LIGHTBLUE_EX+" "+e,"\n")
-	com=(input(Fore.LIGHTBLUE_EX+" passwdb "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX))
-	if com == "0":
-		os.system("clear") if os.name == "posix" else os.system("cls")
-		logo()
-		name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX+"")
-		passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-		print(Fore.LIGHTBLUE_EX+" ok !, please wite ...")
-		import sqlite3
-		try:
-			db=sqlite3.connect(".li.db")
-			ac=db.execute("SELECT * FROM li")
-			sname=name
-		except:
-			print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" account is not exists !")
-			input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-			loginer()
-		for pname,ppasswd,phash in ac:
-			nname=pname
-			npasswd=ppasswd
-			nhash=phash
-		if name == nname:
-			if passwd == npasswd:
-				passwdb(sname)
-			else:
-				print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" name or password is wrong ! ")
-				input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-				loginer()
-		else:
-			print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" name or password is wrong ! ")
-			input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-			loginer()
+    from time import sleep 
+    os.system("clear") if os.name == "posix" else os.system("cls")
+    pwd=os.getcwd()
+    logo()
+    print(Fore.LIGHTBLUE_EX+" WELCOME TO PASSWDB ! :)\n")
+    incm=["[0]- login ","[1]- account"] 
+    for e in incm:
+        print(Fore.YELLOW+" ➜"+Fore.LIGHTBLUE_EX+" "+e,"\n")
+    com=(input(Fore.LIGHTBLUE_EX+" passwdb "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX))
+    if com == "0":
+        os.system("clear") if os.name == "posix" else os.system("cls")
+        logo()
+        name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX+"")
+		#passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+        passwd = getpass(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+        print(Fore.LIGHTBLUE_EX+" ok !, please wite ...")
+        import sqlite3
+        try:
+            db=sqlite3.connect(".li.db")
+            ac=db.execute("SELECT * FROM li")
+            sname=name
+        except:
+            print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" account is not exists !")
+            input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+            loginer()
+        for pname,ppasswd,phash in ac:
+            nname=pname
+            npasswd=ppasswd
+            nhash=phash
+        if name == nname:
+            if passwd == npasswd:
+                passwdb(sname)
+            else:
+                print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" name or password is wrong ! ")
+                input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+                loginer()
+        else:
+            print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" name or password is wrong ! ")
+            input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+            loginer()
 
-	elif com == "1":
-		account()
-	elif com == "exit" or "quit":
-		os.chdir(pwd)
-		sys.exit()
+    elif com == "1":
+        account()
+    elif com == "exit" or "quit":
+        os.chdir(pwd)
+        sys.exit()
 def loginer():
 	os.system("clear")
 	login()
 	
 def passwdb(sname):
-	from time import sleep 
-	pwd=os.getcwd()
-	os.system("clear") if os.name == "posix" else os.system("cls")
-	logo()
-	incm=["[0]- write","[1]- read","[2]- delete"]
-	for e in incm:
-		print(Fore.YELLOW+" ➜"+Fore.LIGHTBLUE_EX+" "+e,"\n")
-		sleep(0.2)
-	com=(input(Fore.LIGHTBLUE_EX+" passwdb "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX))
-	if com == "0":
-		os.system("clear") if os.name == "posix" else os.system("cls")
-		logo()
-		name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX).strip()
-		passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX).strip()
-		print(Fore.LIGHTBLUE_EX+" ok, please wite ...")
-		hash(sname,name,passwd)
-	elif com == "1":
-		sqliteR(sname)
-	elif com == "2":
-		sqliteD(sname)
-	elif com =="exit" or "quit":
-		os.chdir(pwd)
-		sys.exit()
-	else:
-		print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" this is no found !")
-		input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
-		passwdb(sname)
+    from time import sleep 
+    pwd=os.getcwd()
+    os.system("clear") if os.name == "posix" else os.system("cls")
+    logo()
+    incm=["[0]- write","[1]- read","[2]- delete"]
+    for e in incm:
+        print(Fore.YELLOW+" ➜"+Fore.LIGHTBLUE_EX+" "+e,"\n")
+        sleep(0.2)
+    com=(input(Fore.LIGHTBLUE_EX+" passwdb "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX))  
+    if com == "0":
+        os.system("clear") if os.name == "posix" else os.system("cls")
+        logo()
+        name=input(Fore.LIGHTBLUE_EX+" enter name "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX).strip()
+		#passwd=input(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX).strip()
+        passwd = getpass(Fore.LIGHTBLUE_EX+" enter password "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)	
+        print(Fore.LIGHTBLUE_EX+" ok, please wite ...")
+        hash(sname,name,passwd)
+    elif com == "1":
+        sqliteR(sname)
+    elif com == "2":
+        sqliteD(sname)
+    elif com =="exit" or "quit":
+        os.chdir(pwd)
+        sys.exit()
+    else:
+        print(Fore.LIGHTRED_EX+" ➜"+Fore.LIGHTBLUE_EX+" this is no found !")
+        input(Fore.LIGHTBLUE_EX+" press enter to back "+Fore.LIGHTRED_EX+"✗ "+Fore.LIGHTBLUE_EX)
+        passwdb(sname)
 loginer()
 sname=""
 if __name__ == "__passwdb__":passwdb(sname)
